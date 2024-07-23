5fc910d246f81c5189155feeb2d9f33cae666deb
Ekaputripertiwi <2200016133@webmail.uad.ac.id>
2024-07-23 19:53:15 +0700

    update new

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-
+const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
@@ -10,7 +10,7 @@ const paymentRoutes = require('./routes/paymentRoutes');
 const addressRoutes = require('./routes/addressRoutes');
 const indexRoutes = require ('./routes/index');
 dotenv.config();
-
+connectDB();
 // Connect to database
 const app = express();
 
@@ -25,7 +25,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/shippings', shippingRoutes);
 app.use('/api/payments', paymentRoutes);
-app.use('/api/address', addressRoutes);
+app.use('/api/address', addressRoutes);     
 
 const PORT = process.env.PORT || 3000;
 
